e07874432f7c94d5d0c5753efbd5b20c75a38aec
mariepascale <marie.pascale.chauvel@gmail.com>
2019-01-10 10:54:30 -0500

    add app to test buddy build CI

---
+++ buddy-build-deploy/buddy-build-deployTests/buddy_build_deployTests.swift
@@ -0,0 +1,34 @@
+//
+//  buddy_build_deployTests.swift
+//  buddy-build-deployTests
+//
+//  Created by Marie-Pascale Chauvel on 1/10/19.
+//  Copyright © 2019 Marie-Pascale Chauvel. All rights reserved.
+//
+
+import XCTest
+@testable import buddy_build_deploy
+
+class buddy_build_deployTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
